0e02042fb9fa7f4196fff180b72d5ce575bb4e28
Younesbzize0-cloud <younesbzize0@gamil.com>
2026-06-28 16:25:41 +0100

    perf: modified IUser

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -7,12 +7,13 @@ export interface IUser extends Document {
   role: 'user' | 'admin';
 }
 
-
-const userSchema = new Schema<IUser> ({
-    name: { type: String, required: true},
-    email: { type: String, requierd : true, unique: true},
-    password: {type: String, required: true},
-    role: { type: String, enum: ['user', 'admin'], default: 'user'}
+const userSchema = new Schema<IUser>({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true, lowercase: true, trim: true }, 
+  password: { type: String, required: true },
+  role: { type: String, enum: ['user', 'admin'], default: 'user' }
+}, {
+  timestamps: true 
 });
 
 export default mongoose.model<IUser>('User', userSchema);
